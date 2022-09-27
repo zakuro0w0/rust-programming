feebed2adc6e2d778e6c0270c0dba4bfb096ccc8
zakuro0w0 <tatatake0w0@gmail.com>
2022-09-27 21:59:11 +0900

    feat: stateパターンが任意のenumイベントを取り扱えるように変更
    - State<T>は任意の型Tをon_event()で受け取り、次の状態を返すメソッドを提供する

--- design_pattern/state/src/main.rs
+++ design_pattern/state/src/main.rs
@@ -5,12 +5,47 @@ mod state;
 // 自分のcrateのstateモジュール配下全てについて使用宣言する
 use crate::state::*;
 
+/// スイッチON状態
+pub struct On;
+/// スイッチOFF状態
+pub struct Off;
+
+/// イベントの種類
+pub enum Event {
+    /// スイッチ操作されたイベント
+    Switch,
+    /// その他のイベント
+    Other,
+}
+
+/// ON状態にStateトレイトを実装する
+impl State<Event> for On {
+    fn on_event(&self, event: Event) -> Box<dyn State<Event>> {
+        println!("On::switch!!");
+        // ON状態の時にスイッチ操作された
+        // 次の状態としてOFF状態を返す
+        Box::new(Off)
+    }
+}
+
+/// OFF状態にStateトレイトを実装する
+impl State<Event> for Off {
+    fn on_event(&self, event: Event) -> Box<dyn State<Event>> {
+        println!("Off::switch!!");
+        // OFF状態の時にスイッチ操作された
+        // 次の状態としてON状態を返す
+        Box::new(On)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     // 新しいステートマシンを生成(状態が遷移するのでmutableで作る)
-    let mut state_machine = state::StateMachine::new();
+    let mut state_machine = state::StateMachine {
+        current: Box::new(Off),
+    };
     // イベントの発生をステートマシンに通知する度にマシン内部で状態遷移が起こる
-    state_machine.on_event(state::Event::Switch);
-    state_machine.on_event(state::Event::Switch);
-    state_machine.on_event(state::Event::Switch);
+    state_machine.on_event(Event::Switch);
+    state_machine.on_event(Event::Switch);
+    state_machine.on_event(Event::Switch);
 }
