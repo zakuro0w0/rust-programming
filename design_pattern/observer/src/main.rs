f6221e0ac8e0f6641cbb8903eacea6f57f857362
zakuro0w0 <tatatake0w0@gmail.com>
2022-09-29 22:57:06 +0900

    feat: observerパターンをトレイトオブジェクトではなくジェネリクスで再実装
    - ↓を参考にした
      - https://github.com/lpxxn/rust-design-pattern/blob/master/behavioral/observer.rs
    - ジェネリクスの型パラメータでトレイトを表現するため、Box<dyn Trait>で書く必要は無くなった
    - トレイトオブジェクトとジェネリクスどちらで書くべきか、はまた調べる必要がある
    - ジェネリクスにしたことで構造体のフィールドに参照を含むため、生存期間パラメータの記述が必要になった
      - 型とセットで出てくるので出現頻度が高く、慣れないせいか少し煩わしく感じる
    - Observer::notify()で任意の型Tを渡せるようにしておきたい

--- design_pattern/observer/src/main.rs
+++ design_pattern/observer/src/main.rs
@@ -1,38 +1,77 @@
 mod observer;
 use observer::*;
 
-struct News<T> {
-    observers: Vec<Box<dyn Observer<T>>>,
+/// 購読対象となる構造体
+struct News<'a, O: Observer> {
+    /// 購読者リスト
+    /// 生存期間aのObserverトレイト実装の参照を配列として持つ
+    observers: Vec<&'a O>,
 }
 
-impl<T> Subject<T> for News<T> {
-    fn subscribe(&mut self, observer: Box<dyn Observer<T>>) {
+/// News構造体へのSubjectトレイトの実装
+/// unsubscribe()でObserver同士を比較するためPartialEqトレイトも要求する必要がある
+impl<'a, O: Observer + PartialEq> Subject<'a, O> for News<'a, O> {
+    fn subscribe(&mut self, observer: &'a O) {
+        // 購読者をリストに追加する
         self.observers.push(observer);
     }
-}
-
-impl<T> News<T> {
-    fn post(&self, data: &T) {
-        for observer in &self.observers {
-            observer.notify(data);
+    fn unsubscribe(&mut self, observer: &'a O) {
+        // position()で条件に一致する要素のindexをSome(usize)として取得する
+        // 一致する要素が見つかり、if letで指定したパターン(Some(usize))に一致したらブロック内の処理を実行する
+        if let Some(index) = self.observers.iter().position(|x| *x == observer) {
+            // 条件に一致する購読者をリストから削除する
+            self.observers.remove(index);
         }
     }
 }
 
-struct Listener {}
+/// ニュース構造体のメソッド実装
+impl<'a, O: Observer> News<'a, O> {
+    /// ニュース記事を投稿する
+    fn post(&self) {
+        // 購読者リストの全員に通知を行う
+        for observer in &self.observers {
+            observer.notify();
+        }
+    }
+}
 
-impl<T> Observer<T> for Listener {
-    fn notify(&self, data: &T) {
-        println!("new data notified!!");
+/// 購読者の構造体
+#[derive(Debug, PartialEq)]
+struct Listener {
+    /// 購読者の名前
+    name: String,
+}
+
+/// Observerトレイトを購読者に実装する
+impl Observer for Listener {
+    fn notify(&self) {
+        // 更新の通知が来たら名前を出力する
+        println!("{} received subject notify", self.name);
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let listener = Listener {};
-    let mut news = News::<String> {
+    // 購読者: アリス
+    let alice = Listener {
+        name: "Alice".to_string(),
+    };
+    // 購読者: ボブ
+    let bob = Listener {
+        name: "Bob".to_string(),
+    };
+    // ニュース記事
+    let mut news = News {
         observers: Vec::new(),
     };
-    news.subscribe(Box::new(listener));
-    news.post(&"todays weather news!!".to_string());
+    // ニュース記事の購読開始
+    news.subscribe(&alice);
+    news.subscribe(&bob);
+    // ニュース記事の更新(購読者全員に通知される)
+    news.post();
+    // アリスが購読を解除
+    news.unsubscribe(&alice);
+    // ニュース記事の更新(ボブのみに通知される)
+    news.post();
 }
--- design_pattern/observer/src/observer.rs
+++ design_pattern/observer/src/observer.rs
@@ -1,7 +1,14 @@
-pub trait Subject<T> {
-    fn subscribe(&mut self, observer: Box<dyn Observer<T>>);
+/// 生存期間aのObserverトレイト実装型をOとする
+pub trait Subject<'a, O: Observer> {
+    /// Subjectの更新を購読する
+    fn subscribe(&mut self, observer: &'a O);
+    /// Subject更新の購読を解除する
+    fn unsubscribe(&mut self, observer: &'a O);
 }
 
-pub trait Observer<T> {
-    fn notify(&self, data: &T);
+/// Subjectの購読者==Subscriber
+pub trait Observer {
+    /// 購読しているSubjectの更新時に実行される
+    /// まだ任意のデータを渡すようにはできていない
+    fn notify(&self);
 }
