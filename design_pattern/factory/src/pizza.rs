ee4b5aaaa3794112ce02a97c8da60c71c409c71d
zakuro0w0 <tatatake0w0@gmail.com>
2022-10-02 21:25:10 +0900

    feat: factoryパターン ピザの製法をPizzaFactoryに移譲

--- design_pattern/factory/src/pizza.rs
+++ design_pattern/factory/src/pizza.rs
@@ -40,19 +40,29 @@ impl Pizza for GreekPizza {}
 /// Pizzaのトレイトオブジェクトのエイリアス
 type P = Box<dyn Pizza>;
 
+/// ピザファクトリ  
+/// ピザのインスタンス化コードをこの構造体に閉じ込める
+struct PizzaFactory;
+
+impl PizzaFactory {
+    /// menuで指定された種類のピザを新規に作って返す
+    /// 返すピザはBox<dyn Pizza>、つまりトレイトオブジェクトのBoxとなる
+    fn create_pizza(menu: PizzaMenu) -> P {
+        match menu {
+            PizzaMenu::CheesePizza => Box::new(CheesePizza {}),
+            PizzaMenu::PepperoniPizza => Box::new(PepperoniPizza {}),
+            PizzaMenu::MargheritaPizza => Box::new(MargheritaPizza {}),
+            PizzaMenu::VegiePizza => Box::new(VegiePizza {}),
+            PizzaMenu::GreekPizza => Box::new(GreekPizza {}),
+        }
+    }
+}
+
 /// ピザメニューを1つ指定してピザを注文する
 pub fn order_pizza(menu: PizzaMenu) {
     // どのピザを作るかを決める
-    // このコードがピザメニューの変更に対して閉じていない
-    // ピザメニューが増える度にここに分岐とピザの作り方を追加しなければならない
-    // 実際には構造体のインスタンス化はもっと複雑で長いコードになることが予想される
-    let pizza: P = match menu {
-        PizzaMenu::CheesePizza => Box::new(CheesePizza {}),
-        PizzaMenu::PepperoniPizza => Box::new(PepperoniPizza {}),
-        PizzaMenu::MargheritaPizza => Box::new(MargheritaPizza {}),
-        PizzaMenu::VegiePizza => Box::new(VegiePizza {}),
-        PizzaMenu::GreekPizza => Box::new(GreekPizza {}),
-    };
+    // 製法はPizzaFactoryに移譲したのでピザの種類を指定するだけで良い
+    let pizza: P = PizzaFactory::create_pizza(menu);
     pizza.prepare();
     pizza.bake();
     pizza.cut();
